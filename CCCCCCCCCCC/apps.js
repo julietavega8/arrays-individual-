075467c903eb13671470958451c0fbe12040f839
julietavega8 <85358049+julietavega8@users.noreply.github.com>
2022-06-02 09:59:43 -0300

    Add files via upload

---
+++ CCCCCCCCCCC/apps.js
@@ -0,0 +1,20 @@
+
+
+let alumnos= ["Ricardo Fort", "Juana Viale", "Mirtha Legrand", "Mauro Lombardo", "Emilia Mernes"]
+
+document.write(alumnos)
+
+console.log(alumnos [4])
+
+let mirtha= alumnos.slice("Mirtha Legrand")
+
+let sacar= alumnos.push("Alberto Fernandez")
+
+let agregar= alumnos.pop("Alberto Fernandez")
+let principio= alumnos.unshift("Alberto Fernandez")
+let sacraprincipio= alumnos.shift("Alberto Fernandez")
+
+let ingresantes2023= ["Felipe Fort", "Lizy Tagliani", "Justin Bieber"]
+nuevoaño.contact(alumnos+ ingresantes2023)
+
+
